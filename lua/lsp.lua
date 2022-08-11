f9f0dd6e7daf411a32c00c508f1e53b90cad2e73
Anh Viet <anhviet5121pc@gmail.com>
2022-08-11 15:25:06 +0700

    Update

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -40,6 +40,7 @@ local on_attach = function(client, bufnr)
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufots)
     vim.keymap.set('n', '<space>wl', function()
